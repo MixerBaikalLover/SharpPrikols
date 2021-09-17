eec799a6b13ee9e2082c5f6558bbc4faccbbef6e
MixerBaikalLover <mixergamechannel@gmail.com>
2021-09-17 13:49:35 +0300

    Checking if functional is working

--- SharpTraining/Computer/School/Program.cs
+++ SharpTraining/Computer/School/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SecondTry
 {
@@ -6,7 +7,13 @@ namespace SecondTry
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Human pudge = new Human("Padj", "Pudjak", "Pudgers", GenderEnum.MALE, 15);
+            Human teach1 = new Teacher("Kol", "Hitori", "who", GenderEnum.MALE, 65, "RMP", 20);
+            Human stud1 = new Student("Max", "Plakhti", "Pentagon", GenderEnum.FEMALE, 20, 5);
+            School sch = new School("no address", 3, 32, "Itma", new List<Teacher>() {(Teacher) teach1},
+                new List<Student>() {(Student) stud1});
+           // Console.Write(sch.AddEntity(pudge));
+           Console.Write(sch.NextYearAll());
         }
     }
 }
